e9ea871ced82453eea4a2fdc6cbc7e1da5ac875d
sanato96 <sanato96@mail.ru>
2023-03-28 21:13:49 +0300

    Новый билет и задания

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,7 +1,8 @@
 // List of tasks
 const tasks = [
+  //ticket
+  // 1
   {
-    //ticket
     image: "images/image1.jpg",
     task: "Найдите номер посадочного места",
     area: {
@@ -13,6 +14,7 @@ const tasks = [
       y: 0.50
     }
   },
+  // 2
   {
     image: "images/image1.jpg",
     task: "Найдите номер вагона",
@@ -25,6 +27,7 @@ const tasks = [
       y: 0.41
     }
   },
+  // 3
   {
     image: "images/image1.jpg",
     task: "Найдите пункт прибытия",
@@ -37,6 +40,7 @@ const tasks = [
       y: 0.46
     }
   },
+  // 4
   {
     image: "images/image1.jpg",
     task: "Найдите данные документа, на который оформлен билет",
@@ -49,6 +53,86 @@ const tasks = [
       y: 0.55
     }
   },
+  // 5
+  {
+    image: "images/image1.jpg",
+    task: "Найдите место, где располгаются ФИО пассажира",
+    area: {
+      x: 390,
+      y: 16
+    },
+    position: {
+      x: 0.25,
+      y: 0.55
+    }
+  },
+  // 6
+  {
+    image: "images/image1.jpg",
+    task: "Найдите номер поезда",
+    area: {
+      x: 40,
+      y: 16
+    },
+    position: {
+      x: 0.08,
+      y: 0.40
+    }
+  },
+  // 7
+  {
+    image: "images/image1.jpg",
+    task: "Найдите информацию о приобретенном белье",
+    area: {
+      x: 115,
+      y: 16
+    },
+    position: {
+      x: 0.59,
+      y: 0.60 
+    }
+  },
+  // 8
+  {
+    image: "images/image1.jpg",
+    task: "Найдите пункт посадки",
+    area: {
+      x: 160,
+      y: 16
+    },
+    position: {
+      x: 0.08,
+      y: 0.45
+    }
+  },
+  // 9
+  {
+    image: "images/image1.jpg",
+    task: "Найдите краткие дату и время отправления",
+    area: {
+      x: 75,
+      y: 16
+    },
+    position: {
+      x: 0.26,
+      y: 0.41
+    }
+  },
+  // 10
+  {
+    image: "images/image1.jpg",
+    task: "Найдите дату и время прибытия",
+    area: {
+      x: 138,
+      y: 18
+    },
+    position: {
+      x: 0.19,
+      y: 0.69
+    }
+  },
+  // ticket
+  // 1
   {
     image: "images/image2.jpg",
     task: "Найдите номер посадочного места",
@@ -61,6 +145,7 @@ const tasks = [
       y: 0.51
     }
   },
+  // 2
   {
     image: "images/image2.jpg",
     task: "Найдите номер вагона",
@@ -73,6 +158,7 @@ const tasks = [
       y: 0.41
     }
   },
+  // 3
   {
     image: "images/image2.jpg",
     task: "Найдите пункт прибытия",
@@ -85,6 +171,7 @@ const tasks = [
       y: 0.46
     }
   },
+  // 4
   {
     image: "images/image2.jpg",
     task: "Найдите данные документа, на который оформлен билет",
@@ -96,8 +183,8 @@ const tasks = [
       x: 0.04,
       y: 0.61
     }
-  }
-  ,
+  },
+  // 5
   {
     image: "images/image2.jpg",
     task: "Найдите данные купленного белья",
@@ -109,6 +196,201 @@ const tasks = [
       x: 0.47,
       y: 0.72
     }
+  },
+  // 6
+  {
+    image: "images/image2.jpg",
+    task: "Найдите место, где располгаются ФИО пассажира",
+    area: {
+      x: 70,
+      y: 15
+    },
+    position: {
+      x: 0.20,
+      y: 0.61
+    }
+  },
+  // 7
+  {
+    image: "images/image2.jpg",
+    task: "Найдите пункт посадки",
+    area: {
+      x: 65,
+      y: 16
+    },
+    position: {
+      x: 0.04,
+      y: 0.45
+    }
+  },
+  // 8
+  {
+    image: "images/image2.jpg",
+    task: "Найдите пункт высадки",
+    area: {
+      x: 67,
+      y: 16
+    },
+    position: {
+      x: 0.17,
+      y: 0.45
+    }
+  },
+  // 9
+  {
+    image: "images/image2.jpg",
+    task: "Найдите дату и время прибытия",
+    area: {
+      x: 90,
+      y: 15
+    },
+    position: {
+      x: 0.37,
+      y: 0.76
+    }
+  },
+  // 10
+  {
+    image: "images/image2.jpg",
+    task: "Найдите краткие дату и время отправления",
+    area: {
+      x: 83,
+      y: 15
+    },
+    position: {
+      x: 0.14,
+      y: 0.40
+    }
+  },
+  // ticket
+  // 1
+  {
+    image: "images/image3.jpg",
+    task: "Найдите номер посадочного места",
+    area: {
+      x: 75,
+      y: 25
+    },
+    position: {
+      x: 0.24,
+      y: 0.46
+    }
+  },
+  // 2
+  {
+    image: "images/image3.jpg",
+    task: "Найдите номер вагона",
+    area: {
+      x: 47,
+      y: 27
+    },
+    position: {
+      x: 0.39,
+      y: 0.37
+    }
+  },
+  // 3
+  {
+    image: "images/image3.jpg",
+    task: "Найдите пункт прибытия",
+    area: {
+      x: 242,
+      y: 30
+    },
+    position: {
+      x: 0.42,
+      y: 0.41
+    }
+  },
+  // 4
+  {
+    image: "images/image3.jpg",
+    task: "Найдите данные документа, на который оформлен билет",
+    area: {
+      x: 187,
+      y: 27
+    },
+    position: {
+      x: 0.17,
+      y: 0.50
+    }
+  },
+  // 5
+  {
+    image: "images/image3.jpg",
+    task: "Найдите данные купленного белья",
+    area: {
+      x: 125,
+      y: 28
+    },
+    position: {
+      x: 0.47,
+      y: 0.55
+    }
+  },
+  // 6
+  {
+    image: "images/image3.jpg",
+    task: "Найдите место, где располгаются ФИО пассажира",
+    area: {
+      x: 380,
+      y: 30
+    },
+    position: {
+      x: 0.30,
+      y: 0.50
+    }
+  },
+  // 7
+  {
+    image: "images/image3.jpg",
+    task: "Найдите пункт посадки",
+    area: {
+      x: 330,
+      y: 28
+    },
+    position: {
+      x: 0.17,
+      y: 0.41
+    }
+  },
+  // 8
+  {
+    image: "images/image3.jpg",
+    task: "Найдите пункт высадки",
+    area: {
+      x: 240,
+      y: 32
+    },
+    position: {
+      x: 0.42,
+      y: 0.41
+    }
+  },
+  // 9
+  {
+    image: "images/image3.jpg",
+    task: "Найдите дату и время прибытия",
+    area: {
+      x: 210,
+      y: 27
+    },
+    position: {
+      x: 0.26,
+      y: 0.69
+    }
+  },
+  // 10
+  {
+    image: "images/image3.jpg",
+    task: "Найдите краткие дату и время отправления",
+    area: {
+      x: 190,
+      y: 32
+    },
+    position: {
+      x: 0.25,
+      y: 0.36
+    }
   }
-  //online ticket
 ];
